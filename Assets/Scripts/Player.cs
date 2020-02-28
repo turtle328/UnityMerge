93a10706c9694c02ee18c5dd7ce51e39ac101f61
Drew <ddd5189@rit.edu>
2020-02-28 13:28:03 -0500

    good change

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    const string k_HORIZONTAL = "Horizontal";
+    const string good = "Vertical";
+
+    [SerializeField]
+    float moveSpeed = 10;
+
+    Vector3 moveDelta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDelta.x = Input.GetAxis(k_HORIZONTAL);
+        moveDelta.z = Input.GetAxis(k_VERTICAL);
+
+        transform.Translate(moveDelta * moveSpeed * Time.deltaTime);
+    }
+}
