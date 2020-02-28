9d7ff8c5f69fdced4e00c8cce691f6d9b9c001c3
Eric Baker <eab6863@gmail.com>
2020-02-27 19:59:32 -0500

    Added logic to look around

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    const string k_MOUSE_X = "Mouse X";
+    const string k_MOUSE_Y = "Mouse Y";
+
+    [SerializeField]
+    float sensitivity = 10f;
+
+    Vector2 currentRotation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentRotation.x += Input.GetAxis(k_MOUSE_X) * sensitivity;
+        currentRotation.y -= Input.GetAxis(k_MOUSE_Y) * sensitivity;
+
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+
+        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
     }
 }
